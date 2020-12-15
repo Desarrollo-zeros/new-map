dd272126103499570192cb85ecc7d46b46241000
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-12-14 23:01:30 -0500

    correccion de escala fabian

--- js/Api.js
+++ js/Api.js
@@ -1,7 +1,7 @@
 class Api{
 
     constructor() {
-        this.url = "http://localhost/sustain_map/app_modules/maps/API/maps/";
+        this.url = "http://13.59.69.169/sustain_map/app_modules/maps/API/maps/";
         this.response = null;
 
         this.dataVectores= {};
